4cd9e12c203f9d8a8a826ffe9860db90efd0d5b5
Eirik <eiriklin@yahoo.com>
2016-08-05 20:19:53 -0700

    Add - Regex Koans

---
+++ spec/01_Basics.js
@@ -0,0 +1,23 @@
+describe("Basics", function() {
+
+	it('replace /^___$/ with /^a$/ to make RegEx match', function() {
+		expect('a').toMatch(/^a$/);
+	});
+	
+	it('a RegEx that is the same as the string will match', function() {
+		expect('abcd').toMatch(/^abcd$/);
+	});
+	
+	it('RegEx is case sensitive', function() {
+		expect('AbCd').toMatch(/^AbCd$/);
+	});
+	
+	it('whitespace matters', function() {
+		expect('ab cd').toMatch(/^ab\scd$/);
+	});
+	
+	it('testing for does-not-match is often useful, too', function() {
+		expect('abcd').not.toMatch(/^[^abcd]$/);
+	});
+
+});
